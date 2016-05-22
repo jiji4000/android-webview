efbb3306f697ecb15e78c7eac6c412ad6cf1b4df
jiji3000 <tokyo.orinpic@gmail.com>
2016-05-22 18:39:08 +0900

    Webの基礎を追加

---
+++ app/src/main/java/com/example/jiji30000/webview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.jiji30000.webview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.webview);
+        // 外部ブラウザを立ち上げない設定その他いろいろcallbackとか
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl("http://tabelog.com/");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
